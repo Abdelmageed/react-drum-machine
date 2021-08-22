f8acdbd84ef26c23a4227f09aad69b39153cf253
amostafa <abdelmageedsayed@hotmail.com>
2021-08-22 19:15:00 +0200

    Moved sounds to public folder and fixed references.

--- src/App.tsx
+++ src/App.tsx
@@ -1,48 +1,45 @@
-import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 import { Sound } from './models';
 import { DrumPad } from './DrumPad';
-
 
 function App() {
   const sounds: Sound[] = [
     {
       name: 'kick1',
-      path: 'assets/kick-1.wav',
+      path: `${process.env.PUBLIC_URL}/sounds/kick-1.wav`,
       key: 'q'
     }, {
       name: 'kick2',
-      path: 'assets/kick-2.wav',
+      path: `${process.env.PUBLIC_URL}/sounds/kick-2.wav`,
       key: 'w'
     }, {
       name: 'kick3',
-      path: 'assets/kick-3.wav',
+      path: `${process.env.PUBLIC_URL}/sounds/kick-3.wav`,
       key: 'e'
     }, {
       name: 'kick4',
-      path: 'assets/kick-4.wav',
+      path: `${process.env.PUBLIC_URL}/sounds/kick-4.wav`,
       key: 'a'
     }, {
       name: 'clap1',
-      path: 'assets/kick-1.wav',
-      key: 'q'
+      path: `${process.env.PUBLIC_URL}/sounds/clap-1.wav`,
+      key: 's'
     }, , {
       name: 'clap2',
-      path: 'assets/kick-1.wav',
-      key: 'q'
+      path: `${process.env.PUBLIC_URL}/sounds/clap-2.wav`,
+      key: 'd'
     }, {
       name: 'hi-hat-1',
-      path: 'assets/kick-1.wav',
-      key: 'q'
+      path: `${process.env.PUBLIC_URL}/sounds/hi-hat-1.wav`,
+      key: 'z'
     }, {
       name: 'hi-hat-2',
-      path: 'assets/kick-1.wav',
-      key: 'q'
+      path: `${process.env.PUBLIC_URL}/sounds/hi-hat-2.wav`,
+      key: 'x'
     }, {
       name: 'bass',
-      path: 'assets/kick-1.wav',
-      key: 'q'
+      path: `${process.env.PUBLIC_URL}/sounds/bass.wav`,
+      key: 'c'
     }
   ] as Sound[];
 
