dd2c7d65168c31788d4ad56636a26feccb00d077
amostafa <abdelmageedsayed@hotmail.com>
2021-08-22 19:33:20 +0200

    Handle keydown event via react-event-listener & play mapped sound.

--- src/DrumPad.tsx
+++ src/DrumPad.tsx
@@ -1,4 +1,5 @@
 import React, { ReactNode } from "react";
+import EventListener from 'react-event-listener';
 import { Sound } from "./models";
 
 type DrumPadProps = {
@@ -10,12 +11,44 @@ type DrumPadState = {
 };
 
 export class DrumPad extends React.Component<DrumPadProps, DrumPadState> {
+
+    constructor(props: DrumPadProps) {
+        super(props);
+
+        this.handleClick = this.handleClick.bind(this);
+        this.handleKeyDown = this.handleKeyDown.bind(this);
+        this.playSound = this.playSound.bind(this);
+    }
+
+    handleClick() {
+        this.playSound();
+    }
+
+    handleKeyDown(event: KeyboardEvent) {
+        if (event.key.toLowerCase() === this.props.sound.key) {
+            this.playSound();
+        }
+    }
+
+    playSound() {
+        const audio = document.getElementById(this.props.sound.key) as HTMLAudioElement;
+        audio.play();
+    }
+
     render(): ReactNode {
-        return <div className="drum-pad">
-            {this.props.sound.key}
-            <audio
-                id={this.props.sound.key}
-                src={this.props.sound.path}>{this.props.sound.key}</audio>
-        </div>
+        return (
+            <EventListener
+                target="window"
+                onKeyDown={this.handleKeyDown}
+            >
+                <div className="drum-pad" onClick={this.handleClick}>
+                    {this.props.sound.key}
+                    <audio
+                        id={this.props.sound.key}
+                        src={this.props.sound.path}>{this.props.sound.key}
+                        className="clip"</audio>
+                </div>
+            </EventListener>
+        );
     }
 }
